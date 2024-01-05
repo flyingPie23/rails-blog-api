36a98581b177a403c43f219d7892368556f4d9e3
flyingPie23 <drissfreah325@gmail.com>
2024-01-05 19:08:54 +0100

    set up the backend api

--- Gemfile
+++ Gemfile
@@ -51,8 +51,11 @@ gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 
+
 gem "pundit"
 gem 'simple_token_authentication'
+gem 'rack-cors'
+
 
 group :development, :test do
   gem "dotenv-rails"
---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001' # Replace this with the actual origin of your React app
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
